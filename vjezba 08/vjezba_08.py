39ed71fd0ca28ff7cbe9129debd7b174687e1ef5
Josip Seketa <jseketa@student.unipu.hr>
2025-11-02 20:54:19 +0100

    Solved excercise 08 without validating if the list contains only integers.

---
+++ vjezba 08/vjezba_08.py
@@ -0,0 +1,20 @@
+# Vježba 8: Filtriranje parnih iz liste
+
+# Napišite funkciju koja prima listu cijelih brojeva i vraća novu lista koja sadrži samo parne brojeve iz originalne liste.
+
+# Primjer:
+
+# lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(filtriraj_parne(lista)) # [2, 4, 6, 8, 10]
+
+
+def filtriraj_parne(original_list: list[int]) -> list[int]:
+    filtered_list = [n for n in original_list if n % 2 == 0]
+
+    return filtered_list
+
+
+lista: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(filtriraj_parne(lista))
